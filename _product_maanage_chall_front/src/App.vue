8fcc1d8a7a0a0adc3e2495f83123fd16d1d565a2
mohamedchaaba <mohamedchaaba7@gmail.com>
2024-01-21 19:03:42 +0100

    product-manager

--- _product_maanage_chall_front/src/App.vue
+++ _product_maanage_chall_front/src/App.vue
@@ -1,47 +1,134 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="background"></div>
+  <div class="navbar">
+    <nav class="nav">
+      <div class="nav-el" id="name_project">
+        <img src="./assets/logo.png" width="30" class="me-2" alt="" />
+        Product Manager
+      </div>
+      <div class="nav-el">
+        <a
+          href="https://github.com/flavio-back-end/project_manager.git"
+          target="blank"
+          >GitHub Repo</a
+        >
+      </div>
+    </nav>
+  </div>
+  <router-view id="content" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+/* CUSTOM STYLE */
+
+#content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid lightgray;
+  padding: 4rem 4rem;
+  border-radius: 5px;
+  background: #fefefe;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.background {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  background: rgb(250, 250, 250);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.navbar {
+  background: #fafafa;
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  height: 70px;
+  box-shadow: 6.7px 6.7px 5.3px rgba(0, 0, 0, 0.028),
+    22.3px 22.3px 17.9px rgba(0, 0, 0, 0.042),
+    100px 100px 80px rgba(0, 0, 0, 0.07);
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.nav {
+  width: 80%;
+  min-width: 400px;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background: #fafafa;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#name_project {
+  font-weight: 700;
+}
+
+.container {
+  width: 400px;
+  max-width: 95%;
+}
+
+.input {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+}
+
+.input > label {
+  text-align: start;
+}
+
+.input > input {
+  margin-top: 6px;
+  height: 38px !important;
+}
+
+/* From uiverse.io */
+.btn-pers {
+  position: relative;
+  left: 50%;
+  padding: 1em 2.5em;
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 700;
+  color: #000;
+  background-color: #fff;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  outline: none;
+  transform: translateX(-50%);
+}
+
+.btn-pers:hover {
+  background-color: #198754;
+  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+  color: #fff;
+  transform: translate(-50%, -7px);
+}
+
+.btn-pers:active {
+  transform: translate(-50%, -1px);
+}
+
+/*  */
+
+.alternative-option {
+  text-align: center;
+}
+
+.alternative-option > span {
+  color: #0d6efd;
+  cursor: pointer;
+}
+
+#sign_out {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
