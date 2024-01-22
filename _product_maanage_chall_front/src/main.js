f76071b4035030a21f6a7c30c6896f64a9179d41
Mohamed CHAABA <88441166+flavio-back-end@users.noreply.github.com>
2024-01-22 02:13:36 +0100

    Update main.js

--- _product_maanage_chall_front/src/main.js
+++ _product_maanage_chall_front/src/main.js
@@ -4,14 +4,10 @@ import router from "./router";
 
 createApp(App).use(router).mount("#app");
 import { initializeApp } from "firebase/app";
-const firebaseConfig = {
+const passportConfig = {
   apiKey: "AIzaSyBJa1SPjo1Wm0ZgYXZnmdmvoqs_hHlmiXU",
-  authDomain: "vue-firebase-auth-bafc4.firebaseapp.com",
-  projectId: "vue-firebase-auth-bafc4",
-  storageBucket: "vue-firebase-auth-bafc4.appspot.com",
-  messagingSenderId: "728860345284",
-  appId: "1:728860345284:web:b163b8b54d1f40a5c15a91",
+ 
 };
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(passportConfig);
 
 export { app };
